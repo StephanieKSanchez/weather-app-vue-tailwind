d8cf4d941b80e7efc5ba0edbfcbb285c553cb686
Stephanie Sanchez <stephk006@gmail.com>
2022-10-04 13:52:01 -0400

    added icon for branding from font awesome

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -3,7 +3,9 @@
     <nav
       class="container flex flex-col sm:flex-row items-center gap-4 text-white py-6"
     >
-      <div class="flex items-center gap-3 flex-1"></div>
+      <div class="flex items-center gap-3 flex-1">
+        <i class="fa-solid fa-sun"></i>
+      </div>
     </nav>
   </header>
 </template>
