a8428161138c215eef9c744dd48ef7a88b4d015f
Stephanie Sanchez <stephk006@gmail.com>
2022-10-04 13:53:54 -0400

    added class to make text a little bigger

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -4,7 +4,7 @@
       class="container flex flex-col sm:flex-row items-center gap-4 text-white py-6"
     >
       <div class="flex items-center gap-3 flex-1">
-        <i class="fa-solid fa-sun"></i>
+        <i class="fa-solid fa-sun text-2xl"></i>
       </div>
     </nav>
   </header>
