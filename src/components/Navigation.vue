a378afc8fe189addecbb535c21eef29da11a5a88
Stephanie Sanchez <stephk006@gmail.com>
2022-10-04 13:36:00 -0400

    added some classes

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,5 +1,11 @@
 <template>
-  <header></header>
+  <header class="sticky top-0 bg-weather-primary shadow-lg">
+    <nav
+      class="container flex flex-col sm:flex-row items-center gap-4 text-white py-6"
+    >
+      <div class="flex items-center gap-3 flex-1"></div>
+    </nav>
+  </header>
 </template>
 
 <script setup></script>
