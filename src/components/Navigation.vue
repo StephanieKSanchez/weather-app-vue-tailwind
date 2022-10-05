475d152b824cf12f1f091fbb9d0a2334fe0933d1
Stephanie Sanchez <stephk006@gmail.com>
2022-10-05 13:03:34 -0400

    when user clicks on any part of branding, navigate back to home screen using RouterLink

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -3,12 +3,16 @@
     <nav
       class="container flex flex-col sm:flex-row items-center gap-4 text-white py-6"
     >
-      <div class="flex items-center gap-3 flex-1">
-        <i class="fa-solid fa-sun text-2xl"></i>
-        <p class="text-2xl">The Local Weather</p>
-      </div>
+      <RouterLink :to="{ name: 'home' }">
+        <div class="flex items-center gap-3 flex-1">
+          <i class="fa-solid fa-sun text-2xl"></i>
+          <p class="text-2xl">The Local Weather</p>
+        </div>
+      </RouterLink>
     </nav>
   </header>
 </template>
 
-<script setup></script>
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
